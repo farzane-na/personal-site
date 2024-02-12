7dacf6f02d64976afe426ba086174ce9d29d76a6
Farzane Nazmabadi <1379farzanenazmabadi@gmail.com>
2024-02-13 01:34:56 +0330

    add swiper library and creat slider

--- script/script.js
+++ script/script.js
@@ -1,6 +1,9 @@
 const switchElement = document.querySelector('.dark-mode-switch');
 const body=document.querySelector("body");
 const menuItem=document.querySelectorAll(".item")
+const slider=document.querySelectorAll(".swiper-slide")
+const scrollProgress = document.getElementById('scroll-progress');
+const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 switchElement.addEventListener('click', function () {
     document.body.classList.toggle("dark");
     if(document.body.className.includes("dark")){
@@ -8,13 +11,32 @@ switchElement.addEventListener('click', function () {
         menuItem.forEach(function(item){
             item.style.color="#B19470";
         })
+        slider.forEach(function(slide){
+            slide.style.backgroundColor="#362222"
+        })
+        scrollProgress.style.backgroundColor="#D1EDA3";
     }else{
         localStorage.setItem("theme","light")
         menuItem.forEach(function(item){
             item.style.color="#000";
         })
+        slider.forEach(function(slide){
+            slide.style.backgroundColor="#fff"
+        })
+        scrollProgress.style.backgroundColor="#2d4624";
     }
 })
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    centeredSlides: true,
+    spaceBetween: 30,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
 window.addEventListener("load",function(){
     let localStoragetheme=localStorage.getItem("theme");
     if(localStoragetheme==="dark"){
@@ -22,8 +44,7 @@ window.addEventListener("load",function(){
     }
 })
 
-const scrollProgress = document.getElementById('scroll-progress');
-const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
 
 window.addEventListener('scroll', () => {
   const scrollTop =document.body.scrollTop || document.documentElement.scrollTop;
